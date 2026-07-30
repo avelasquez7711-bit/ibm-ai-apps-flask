60b53060fbd58ba397ef88b526c2312babd2135c
Angel Velasquez <avelasquez7711@gmail.com>
2026-07-30 16:39:38 -0700

    Added all files needed to implement Math Problem Solver webapp - __init__.py - index.html - mathematics.py - mywebscript.js - server.py

---
+++ labs/build-deploy-webapp-flask/mathematics.py
@@ -0,0 +1,14 @@
+# Mathematical Operations
+# Addition
+def summation(a, b):
+    return a + b
+
+
+# Subtraction
+def subtraction(a, b):
+    return a - b
+
+
+# Multiplication
+def multiplication(a, b):
+    return a * b
