42fd15447a41e2dea319019fdb4df33146c291d2
Angel Velasquez <avelasquez7711@gmail.com>
2026-07-31 20:57:34 -0700

    Add fully implemented server.py file - Establishes URL routes, API endpoints for webapp - Flask framework

---
+++ projects/sentiment-analysis/server.py
@@ -0,0 +1,47 @@
+"""Executing this function initiates the application of sentiment
+analysis to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
+
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+
+app = Flask("Sentiment Analyzer")
+
+
+@app.route("/sentimentAnalyzer")
+def sent_analyzer():
+    """This code receives the text from the HTML interface and
+    runs sentiment analysis over it using sentiment_analysis()
+    function. The output returned shows the label and its confidence
+    score for the provided text.
+    """
+    # get text to anaylze from request
+    text_to_analyze = request.args.get("textToAnalyze")
+    if text_to_analyze.strip() == "":
+        return "Empty input! Try again."
+
+    # pass text to sentiment_analyzer function and store response
+    response = sentiment_analyzer(text_to_analyze)
+
+    # get label and score from response
+    label = response["label"]
+    score = response["score"]
+
+    if label is None:
+        return "Invalid input! Try again."
+
+    # return formatted string with sentiment label and score
+    return f"The given text has been identified as {label} with a score of {score}."
+
+
+@app.route("/")
+def render_index_page():
+    """This function initiates the rendering of the main application
+    page over the Flask channel
+    """
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
